fae389a1d33b884986f5eda66a92ae31fa19c7b1
Alex Casadevall <alex@lexartlabs.com>
2021-10-26 20:19:25 -0300

    Update notes

---
+++ clase01/app.js
@@ -0,0 +1,14 @@
+let nombre = "Alex"
+let edad   = 29
+let cond   = true
+let lista  = [1, 2, 3]
+let persona = {
+	nombre: "Alex",
+	apellido: "Casadevall"
+}
+
+console.log(nombre)
+console.log(edad)
+console.log(cond)
+console.log(lista)
+console.log(persona)
