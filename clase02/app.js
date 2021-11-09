642698cfa41a43cb44a69db52dbe5fd7aec08cda
Alex Casadevall <alex@lexartlabs.com>
2021-11-09 20:56:10 -0300

    add productos api

--- clase02/app.js
+++ clase02/app.js
@@ -13,6 +13,40 @@ app.get('/hello-world', (req, res) => {
   res.send('Hola World!')
 })
 
+// Todos los productos
+/*
+  - URL: API.com/productos
+          -> {host}/{model}/{null}
+        - Metodo: GET
+        - Input: null
+        - Output: 
+          - Respuesta correcta: {"response":[{...}, ...]}
+          - Respuesta error: {"error": "No se obtuvo productos"}
+*/
+
+app.get('/productos', (req, res) => {
+  let productos = [
+    {"cod":"ABC123", "nombre":"Manzana", "precio":10},
+    {"cod":"ABC111", "nombre":"Pera", "precio":10}
+  ]
+
+  // Salida
+  // content-type de la salida
+  // Defino los headers
+  res.set({
+    'Content-Type': 'application/json'
+  })
+  let respuesta = {error: "No existe productos"}
+  
+  if(productos.length > 0){
+    respuesta = {
+      response: productos
+    }
+  }
+  // Defino el body de la salida
+  res.send(respuesta)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
